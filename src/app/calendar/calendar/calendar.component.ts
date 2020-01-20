29e08f521dc0b826e9ef28d2c98718b15b768141
Andrei Rusu <andrei.rusu2094@gmail.com>
2020-01-20 21:49:01 +0200

    Style the selectors and save the current year and month

--- src/app/calendar/calendar/calendar.component.ts
+++ src/app/calendar/calendar/calendar.component.ts
@@ -11,22 +11,43 @@ export class CalendarComponent implements OnInit {
   years: number[] = [];
   selectedYear: number;
   selectedMonth: string;
+  selectedDate: Date;
+  calendar: number[] = [];
 
   constructor() {
   }
 
   ngOnInit() {
-    let date = new Date();
+    this.selectedDate = new Date();
 
     this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
       'September', 'October', 'November', 'December'];
 
-    let currentYear = date.getFullYear();
+    let currentYear = this.selectedDate.getFullYear();
     this.selectedYear = currentYear;
-    this.selectedMonth = this.months[date.getMonth()];
+    this.selectedMonth = this.months[this.selectedDate.getMonth()];
 
     for (let i = currentYear - 5; i < currentYear + 5; i++) {
       this.years.push(i);
+    }
+  }
+
+  public generateCalendarByMonthSelection(month: string): void {
+    this.calendar = [];
+    this.selectedMonth = month;
+    this.selectedDate = new Date(this.selectedYear, this.months.findIndex(m => m === month));
+
+    for (let i = 0; i < this.selectedDate.getDay(); i++) {
+      this.calendar.push(i);
+    }
+  }
+
+  public generateCalendarByYearSelection(year: number): void {
+    this.selectedYear = year;
+    this.selectedDate = new Date(year, this.months.findIndex(month => month === this.selectedMonth));
+
+    for (let i = 0; i < this.selectedDate.getDay(); i++) {
+      this.calendar.push(i);
     }
   }
 
